2a1e56f7c93877101c1aacde28dd70168f4073a9
Sukkergris <mauritsjpallesen@gmail.com>
2017-02-22 23:56:16 +0100

    Castling still not working

--- TUI/Program.cs
+++ TUI/Program.cs
@@ -9,7 +9,7 @@ namespace TUI
 		{
 			var testBoard = new Board();
 			int turn = 0;
-			Console.Title = "Chess v.1.1 - No auto check for remi and no king/rook move.";
+			Console.Title = "Chess v.1.1 - No auto check for remi and no castling.";
 			bool checkmate = false;
 
 			while (!checkmate) {
@@ -63,20 +63,22 @@ namespace TUI
 
 				//Restricts moving other teams pieces
 				int check = Move.FindPiece (testBoard.Pieces, a, b);
-				if (check > -1) {
-					if (turn % 2 == 0) {
-						if (testBoard.Pieces [check].Color != "White") 
-						{
-							continue;
-						}
-					} 
-					else 
+				if (check < 0) {
+					continue;
+				}
+
+				if (turn % 2 == 0) {
+					if (testBoard.Pieces [check].Color != "White") 
 					{
-						if (testBoard.Pieces [check].Color != "Black") 
-						{
-							continue;
-						}	
+						continue;
 					}
+				} 
+				else 
+				{
+					if (testBoard.Pieces [check].Color != "Black") 
+					{
+						continue;
+					}	
 				}
 
 				Console.Write (" to ");
@@ -84,13 +86,58 @@ namespace TUI
 				int d = ValidateIntInput ();
 				Console.Write ("\n");
 
-				//Restrick invalid moves by coordinates
-				if (testBoard.ValidateMoves (a, b, c, d) == 0) 
-				{
+				if (a == c && b == d) {
 					continue;
 				}
-					
+
+				int cstPTI = Move.FindPiece (testBoard.Pieces, c, d);
+				Piece cstP = testBoard.Pieces[check];
+				Piece cstPT = new Piece();
+				int xDist = Math.Abs (cstP.X - cstPT.X);
+				bool checkCst;
+				bool checkCst1;
+				bool checkCst2;
+				bool checkCst3;
+				bool checkCst4;
+//				bool castling = false;
+
+				if (cstPTI > -1) {
+					cstPT = testBoard.Pieces [cstPTI];
+					checkCst = cstP.Moved == Piece.StateMoved.Unmoved;
+					checkCst1 = cstPT.Moved == Piece.StateMoved.Unmoved;
+					checkCst2 = cstP.Type == "King";
+					checkCst3 = cstPT.Type == "Rook";
+					checkCst4 = cstPT.Color == cstP.Color;
+					bool finalCst = true;
+					if (checkCst && checkCst1 && checkCst2 && checkCst3 && checkCst4) {
+						xDist = Math.Abs (cstP.X - cstPT.X);
+						if (xDist == 4) 
+						{
+							for (int i = 1; i < xDist; i++) {
+								if (Move.FindPiece(testBoard.Pieces, (char)(cstPT.X+i), cstPT.Y) > -1) {
+									finalCst = false;
+								}
+							}
+						} 
+						else 
+						{
+							for (int i = 1; i < xDist; i++) {
+								if (Move.FindPiece(testBoard.Pieces, (char)(cstP.X+i), cstP.Y) > -1) {
+									finalCst = false;
+								}
+							}
+						}
+					}
+					castling = finalCst;
+				}
+
+				//Restrick invalid moves by coordinates
+				if (testBoard.ValidateMoves (a, b, c, d) == 0) {
+					continue;
+				} 
+
 				testBoard.ExecuteMoves (a, b, c, d);
+
 
 				cwk = Move.CheckHelp (testBoard.Pieces, wk);
 				cbk = Move.CheckHelp (testBoard.Pieces, bk);
@@ -100,6 +147,7 @@ namespace TUI
 				{
 					if (!cwk) 
 					{
+						//If castling, execute different move.
 						testBoard.ExecuteMoves (c, d, a, b);
 						continue;
 					}

af35aa319dac5a886c6b3b7d31a44776f8b91345
Sukkergris <mauritsjpallesen@gmail.com>
2017-02-22 12:31:21 +0100

    First commit

--- TUI/Program.cs
+++ TUI/Program.cs
@@ -10,8 +10,21 @@ namespace TUI
 			var testBoard = new Board();
 			int turn = 0;
 			Console.Title = "Chess v.1.0 - No autocheck for check mate";
+			bool checkmate = false;
 
-			while (true) {
+			// foreach p in pl 
+			// 		if p.Color == CheckColor
+			// 			foreach f in fl
+			// 				if testBoard.ValidateMoves(p.X, p.Y, f.X, f.Y)
+			// 					execute move(a, b, c, d)
+			// 					if Move.CheckHelp
+			// 						execute move(c, d, a, b)
+			// 						checkmate = true
+			// 						return
+			//					execute move(c, d, a, b)
+
+
+			while (!checkmate) {
 				Console.Clear ();
 				testBoard.DrawBoard ();
 				int tmp = Move.FindPieceByType (testBoard.Pieces, "King", "White");
@@ -20,6 +33,7 @@ namespace TUI
 				Piece bk = testBoard.Pieces [tmp1];
 				bool cwk = Move.CheckHelp (testBoard.Pieces, wk);
 				bool cbk = Move.CheckHelp (testBoard.Pieces, bk);
+
 				//Prints according to player turn
 				Console.WriteLine (string.Format ("\tMove: {0}", turn + 1));
 				if (turn % 2 == 0) {
@@ -52,6 +66,7 @@ namespace TUI
 						}	
 					}
 				}
+
 				Console.Write (" to ");
 				char c = ValidateCharInput ();
 				int d = ValidateIntInput ();
@@ -66,6 +81,7 @@ namespace TUI
 
 				cwk = Move.CheckHelp (testBoard.Pieces, wk);
 				cbk = Move.CheckHelp (testBoard.Pieces, bk);
+
 				//Restrict moves that puts your king in chess on your own turn.
 				if (turn % 2 == 0) {
 					if (!cwk) {
